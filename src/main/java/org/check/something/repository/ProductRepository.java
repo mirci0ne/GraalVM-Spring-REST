f9110f80aafcc559175c26abec07fb8cfceab045
Mircea Manole <mircea1406@gmail.com>
2023-05-20 16:26:26 +0300

    feature/implementation-of-product-stock-update-when-order-is-saved: - changed name of addProduct to addOrder - implemented method to update product stock - added in product entity unique column property - added in repository method to find product by name - removed product response

--- src/main/java/org/check/something/repository/ProductRepository.java
+++ src/main/java/org/check/something/repository/ProductRepository.java
@@ -2,6 +2,12 @@ package org.check.something.repository;
 
 import org.check.something.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = "select * from product where product_name = :productName", nativeQuery = true)
+    Product getIdByProductName(@Param("productName") String productName);
+
 }
--- src/main/java/org/check/something/services/OrdersServiceImpl.java
+++ src/main/java/org/check/something/services/OrdersServiceImpl.java
@@ -4,13 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.check.something.dtos.OrdersDto;
 import org.check.something.entities.Clients;
 import org.check.something.entities.Orders;
+import org.check.something.entities.Product;
 import org.check.something.mappers.ClientsMapper;
 import org.check.something.mappers.OrdersMapper;
 import org.check.something.repository.ClientsRepository;
 import org.check.something.repository.OrdersRepository;
+import org.check.something.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -19,8 +22,13 @@ public class OrdersServiceImpl {
 
     private final OrdersRepository ordersRepository;
     private final ClientsRepository clientsRepository;
+    private final ProductRepository productRepository;
 
     public Orders save(OrdersDto ordersDto) {
+        Product product = productRepository.getIdByProductName(ordersDto.getOrderProduct());
+        Objects.requireNonNull(product, "Product not found");
+        product.setStock(product.getStock() - ordersDto.getAmount());
+        productRepository.save(product);
         Clients clientsDto = ClientsMapper.MAPPER.ordersToClients(ordersDto);
         Orders orders = OrdersMapper.MAPPER.ordersDtoToOrders(ordersDto);
         clientsRepository.save(clientsDto);
